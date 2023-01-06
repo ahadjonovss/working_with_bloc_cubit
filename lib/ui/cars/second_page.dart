41eea579ecf9e6142412ca3912cfadb1089a9fbd
Samandar <samandarahadjonov@gmail.com>
2023-01-06 17:37:29 +0500

    model done

--- lib/ui/cars/second_page.dart
+++ lib/ui/cars/second_page.dart
@@ -8,48 +8,60 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: BlocConsumer<CarsCubit,CarsState>(
-        builder:(context, state) {
-          if(state is InitialState){
-            return Center(
-              child: Text("No data"),
-            );
-          }
-          if(state is LoadCarsInProgress){
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if(state is LoadCarsInFailure){
-            return Text(state.errorText,);
-          }
-          if(state is LoadCarsInSuccess){
-            return ListView(
-              children: List.generate(
-                state.cars.data.length,
-                    (index) => ListTile(
-                  title: Text(
-                    state.cars.data[index].carModel,
+    return BlocProvider(
+      create: (context) => CarsCubit(),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: BlocConsumer<CarsCubit, CarsState>(
+          builder: (context, state) {
+            if (state is InitialState) {
+              return Column(children: [
+                Center(
+                  child: Text("No data"),
+                ),
+                IconButton(
+                    onPressed: () {
+                      context.read<CarsCubit>().fetchCars();
+                    },
+                    icon: Icon(Icons.update)),
+              ]);
+            }
+            if (state is LoadCarsInProgress) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is LoadCarsInFailure) {
+              return Text(
+                state.errorText,
+              );
+            }
+            if (state is LoadCarsInSuccess) {
+              return ListView(
+                children: List.generate(
+                  state.cars.data.length,
+                  (index) => ListTile(
+                    title: Text(
+                      state.cars.data[index].carModel,
+                    ),
                   ),
                 ),
-              ),
-            );
-          }
-          return Container();
-
-        },
-        listener: (context, state) {
-          if(state is LoadCarsInSuccess){
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${state.cars.data.length} ta malumot keldi")));
-          }
-
-        },
+              );
+            }
+            return IconButton(
+                onPressed: () {
+                  context.read<CarsCubit>().fetchCars();
+                },
+                icon: Icon(Icons.update));
+          },
+          listener: (context, state) {
+            if (state is LoadCarsInSuccess) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text("${state.cars.data.length} ta malumot keldi")));
+            }
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        context.read<CarsCubit>().fetchCars();
-      }),
     );
   }
 }
